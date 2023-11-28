5e0512427ae49c134e02208c09500e0294760b4f
daredevil <himanshu12.gupta@ril.com>
2023-11-28 23:34:34 +0530

    fix: button compoent fix

--- js/Button/Button.js
+++ js/Button/Button.js
@@ -1,57 +1,168 @@
-/* eslint-disable react/button-has-type */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styled from 'styled-components';
 import cx from 'classnames';
 
+const styles = {
+  color(props) {
+    if (props.kind === 'filled') {
+      return props.theme.color.white;
+    }
+    if (props.kind === 'outlined') {
+      return props.theme.color[props.color];
+    }
+    return '';
+  },
+  backgroundColor(props) {
+    if (props.kind === 'filled') {
+      return props.theme.color[props.color];
+    }
+    if (props.kind === 'outlined') {
+      return props.theme.color.white;
+    }
+    return '';
+  },
+  fontSize(props) {
+    if (props.size === 'small') {
+      return props.theme.fontSize.xs;
+    }
+    if (props.size === 'medium') {
+      return props.theme.fontSize.s;
+    }
+    if (props.size === 'large') {
+      return props.theme.fontSize.s;
+    }
+    return '';
+  },
+  padding(props) {
+    if (props.size === 'small') {
+      return props.theme.px([1, 2]);
+    }
+    if (props.size === 'medium') {
+      return props.theme.px([1.5, 2]);
+    }
+    if (props.size === 'large') {
+      return props.theme.px([2]);
+    }
+    return '';
+  },
+  borderColor(props) {
+    if (props.kind === 'filled') {
+      return props.theme.color[props.color];
+    }
+    if (props.kind === 'outlined') {
+      return props.theme.color[props.color];
+    }
+    return '';
+  },
+  borderRadius(props) {
+    if (props.shape === 'bluntEdged') {
+      return props.theme.borderRadius;
+    }
+    if (props.shape === 'sharpEdged') {
+      return '0';
+    }
+    if (props.shape === 'capsular') {
+      return props.theme.px(10);
+    }
+    if (props.shape === 'circular') {
+      return '100%';
+    }
+    return '';
+  },
+  pointerEvents(props) {
+    if (props.disabled) {
+      return 'none';
+    }
+    return '';
+  },
+  opacity(props) {
+    if (props.disabled) {
+      return '0.5';
+    }
+    return '';
+  },
+  hover: {
+    color(props) {
+      if (props.kind === 'filled') {
+        return props.theme.color.white;
+      }
+      if (props.kind === 'outlined') {
+        return props.theme.color.white;
+      }
+      return '';
+    },
+    backgroundColor(props) {
+      if (props.kind === 'filled') {
+        return props.theme.color[`${props.color}Dark`];
+      }
+      if (props.kind === 'outlined') {
+        return props.theme.color[props.color];
+      }
+      return '';
+    },
+    borderColor(props) {
+      if (props.kind === 'filled') {
+        return props.theme.color[`${props.color}Dark`];
+      }
+      if (props.kind === 'outlined') {
+        return props.theme.color[props.color];
+      }
+      return '';
+    },
+  },
+};
+
 const StyledButton = styled(
   ({
-    children,
+    color,
+    kind,
+    size,
+    shape,
+    block,
+    isLoading,
     ...props
-  }) => React.createElement('button', { ...props }, children),
+  }) => <button {...props} />,
 )`
-  text-align: center;
-  padding: 12px 36px;
-  background: #03567b;
-  color: white;
-  width: 100%;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  text-transform: uppercase;
+  color: ${styles.color};
+  font-size: ${styles.fontSize};
+  background-color: ${styles.backgroundColor};
+  padding: ${styles.padding};
+  width: ${(props) => props.block ? '100%' : ''};
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${styles.borderColor};
+  border-radius: ${styles.borderRadius};
+  pointer-events: ${styles.pointerEvents};
+  opacity: ${styles.opacity};
 
-  :disabled {
-    opacity: 0.6;
-    cursor: auto;
+  &:hover {
+    color: ${styles.hover.color};
+    background-color: ${styles.hover.backgroundColor};
+    border-color: ${styles.hover.borderColor};
   }
 `;
 
-export default function Button({
-  label,
-  className,
-  clickCbk,
-  disabled,
-  type,
-}) {
-  const buttonClicked = () => {
-    if (!disabled) {
-      clickCbk();
-    }
-  };
+export default function Button({ label, disabled, className, clickCbk, ...props }) {
+  // const buttonClicked = () => {
+  //   if (!disabled) {
+  //     clickCbk();
+  //   }
+  // };
 
   return (
     <StyledButton
-      type={type}
-      disabled={disabled ? 'disabled' : ''}
-      onClick={() => buttonClicked()}
+      {...props}
+      disabled={disabled}
       className={cx(className)}
+      onClick={clickCbk}
     >
-      {
-        disabled ? (
-          <FontAwesomeIcon
-            icon="fa-solid fa-spinner"
-            spin
-            className="mr__8"
-          />
-        ) : null
-      }
+
       {label}
 
     </StyledButton>
@@ -67,8 +178,16 @@ Button.propTypes = {
   className: PropTypes.string,
   clickCbk: () => {},
   disabled: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  kind: PropTypes.oneOf(['filled', 'outlined']),
+  shape: PropTypes.oneOf(['bluntEdged', 'sharpEdged', 'capsular', 'circular']),
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  fluid: PropTypes.bool,
 };
 
 Button.defaultProps = {
   type: 'submit',
+  kind: 'filled',
+  size: 'medium',
+  shape: 'bluntEdged',
 };
