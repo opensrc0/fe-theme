6cf663619e5f523448cb7ef466d96a4bd2ebd733
daredevil <himanshu12.gupta@ril.com>
2023-12-06 17:56:27 +0530

    added sample-theme.js

---
+++ sample-theme.js
@@ -0,0 +1,24 @@
+const theme = {};
+
+theme.color = {
+  primary: '#00364e',
+  secondary: '#03567b',
+
+  white: '#ffffff',
+  black: '#000000',
+
+  info: '#1976d2',
+  success: '#43a047',
+  warning: '#ffa000',
+  danger: '#d32f2f',
+  orange: '#fc6027',
+};
+
+theme.fontFamily = {};
+theme.fontSize = {};
+theme.fontWeight = {};
+theme.display = {};
+theme.borderRadius = '';
+theme.pxScale = '';
+
+export default theme;
