76bc18c9019507d8abe1dfe7a372748a159e275e
Alok <alok1.dubey@ril.com>
2023-12-08 13:51:13 +0530

    added the storybook for button component

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: [
+    "../__application/**/**/*.mdx",
+    "../__application/**/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {
+      builder: {
+        useSWC: true,
+      },
+    },
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
