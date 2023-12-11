75542a65ad948e2c642e8b5c1bbd49fb54698989
daredevil <himanshu12.gupta@ril.com>
2023-12-11 22:13:43 +0530

    fix- build folder

--- Button/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from '../__build-es/Button';
-export * from '../__build-es/Button';
--- Flex/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from '../__build-es/Flex';
-export * from '../__build-es/Flex';
