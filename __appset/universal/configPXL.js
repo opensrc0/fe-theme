37f01859aab571efb1a17c188fc6653322269a71
daredevil <himanshu12.gupta@ril.com>
2023-12-24 16:03:23 +0530

    update readme.md

--- __appset/universal/configPXL.js
+++ __appset/universal/configPXL.js
@@ -3,7 +3,7 @@ const pxScale = 8;
 const PXL = (value) => {
   const values = [].concat(value);
   return values
-    .map((v) => typeof v === 'string' ? v : `${v * pxScale}px`)
+    .map((v) => (typeof v === 'string' ? v : `${v * pxScale}px`))
     .join(' ');
 };
 
