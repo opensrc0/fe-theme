f26592d2ac0ed04fee49cbe1211be6b35bcaf0c4
swaraj <swaraj.singh@ril.com>
2024-01-17 17:02:34 +0530

    removed commented code

--- __application/component/ToggleSwitch/ToggleSwitch.js
+++ __application/component/ToggleSwitch/ToggleSwitch.js
@@ -189,77 +189,36 @@ function ToggleSwitch({
   }
 
   return (
-    <>
-      <ToggleWrapper small={small}>
-        <StyledCheckBox
-          name={name}
-          id={id}
-          checked={checked}
-          onChange={(e) => onChange(e.target.checked)}
-          disabled={disabled}
-        />
-        {id ? (
-          <StyledLabel
-            tabIndex={disabled ? -1 : 1}
-            onKeyDown={(e) => handleKeyPress(e)}
-            htmlFor={id}
-            className="toggle-switch-label"
-          >
-            <StyledSwitchInner
-              disabled={disabled}
-              data-yes={optionLabels[0]}
-              data-no={optionLabels[1]}
-              tabIndex={-1}
-              className="toggle-switch-inner"
-            />
-            <StyledSwitchOuter
-              tabIndex={-1}
-              disabled={disabled}
-              className="toggle-switch-switch"
-            />
-          </StyledLabel>
-        ) : null}
-      </ToggleWrapper>
-
-      {/* <div className={`toggle-switch${small ? ' small-switch' : ''}`}>
-        <input
-          type="checkbox"
-          name={name}
-          className="toggle-switch-checkbox"
-          id={id}
-          checked={checked}
-          onChange={(e) => onChange(e.target.checked)}
-          disabled={disabled}
-        />
-        {id ? (
-          <label
-            className="toggle-switch-label"
-            tabIndex={disabled ? -1 : 1}
-            onKeyDown={(e) => handleKeyPress(e)}
-            htmlFor={id}
-          >
-            <span
-              className={
-              disabled
-                ? 'toggle-switch-inner toggle-switch-disabled'
-                : 'toggle-switch-inner'
-            }
-              data-yes={optionLabels[0]}
-              data-no={optionLabels[1]}
-              tabIndex={-1}
-            />
-            <span
-              className={
-              disabled
-                ? 'toggle-switch-switch toggle-switch-disabled'
-                : 'toggle-switch-switch'
-            }
-              tabIndex={-1}
-            />
-          </label>
-        ) : null}
-      </div> */}
-    </>
+    <ToggleWrapper small={small}>
+      <StyledCheckBox
+        name={name}
+        id={id}
+        checked={checked}
+        onChange={(e) => onChange(e.target.checked)}
+        disabled={disabled}
+      />
+      {id ? (
+        <StyledLabel
+          tabIndex={disabled ? -1 : 1}
+          onKeyDown={(e) => handleKeyPress(e)}
+          htmlFor={id}
+          className="toggle-switch-label"
+        >
+          <StyledSwitchInner
+            disabled={disabled}
+            data-yes={optionLabels[0]}
+            data-no={optionLabels[1]}
+            tabIndex={-1}
+            className="toggle-switch-inner"
+          />
+          <StyledSwitchOuter
+            tabIndex={-1}
+            disabled={disabled}
+            className="toggle-switch-switch"
+          />
+        </StyledLabel>
+      ) : null}
+    </ToggleWrapper>
   );
 }
 
